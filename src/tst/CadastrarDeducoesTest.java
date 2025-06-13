3d403091d47a125d3387ab83e7ca6c7e530d08b1
storch7 <guilhermestorch7@gmail.com>
2025-06-12 22:22:30 -0300

    Métodos para triangulação. 3 succeed

--- src/tst/CadastrarDeducoesTest.java
+++ src/tst/CadastrarDeducoesTest.java
@@ -17,8 +17,19 @@ class CadastrarDeducoesTest {
 	
 	@Test
 	void testCadastrarPrevidenciaOficial() {
-		irpf.cadastrarPrevidenciaOficial(700f);
+		irpf.cadastrarPrevidenciaOficial(700);
 		assertEquals(700, irpf.getPrevidenciaOficial(), 0);
 	}
-
+	
+	@Test
+	void testCadastrarPrevidenciaOficial2() {
+		irpf.cadastrarPrevidenciaOficial(800);
+		assertEquals(800, irpf.getPrevidenciaOficial(), 0);
+	}
+	
+	@Test
+	void testCadastrarPrevidenciaOficial3() {
+		irpf.cadastrarPrevidenciaOficial(900);
+		assertEquals(900, irpf.getPrevidenciaOficial(), 0);
+	}
 }
