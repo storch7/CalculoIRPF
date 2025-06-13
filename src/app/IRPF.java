7d3689fcda961877c95b5c82ded0fe52d5bfecce
storch7 <guilhermestorch7@gmail.com>
2025-06-12 22:32:16 -0300

    Primeira modificação do método cadastrar dependentes

--- src/app/IRPF.java
+++ src/app/IRPF.java
@@ -4,6 +4,7 @@ public class IRPF {
 	
 	private float salario;
 	private float contribuicaoPrevidenciaria;
+	private String[] dependentes;
 		
 	public void cadastrarSalario(float salario) {
 		this.salario += salario;
@@ -19,5 +20,19 @@ public class IRPF {
 	
 	public float getPrevidenciaOficial() {
 		return this.contribuicaoPrevidenciaria;
+	}
+	
+	public void cadastrarDependente(String nome) {
+		//criar uma nova posição no vetor de dependentes
+		String[] temp = new String[dependentes.length + 1];
+		for (int i = 0; i < dependentes.length; i++) {
+			temp[i] = dependentes[i];
+		}
+		
+		//Alocar novo dependente no vetor
+		temp[dependentes.length] = nome;
+		
+		//Atualizar referencias do vetor de dependentes
+		dependentes = temp;
 	}
 }
