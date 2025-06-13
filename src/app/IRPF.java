0df31f2186d8831812f4044e51843c1da9c7e0e1
storch7 <guilhermestorch7@gmail.com>
2025-06-12 21:59:05 -0300

    Técnica de falsificação getTOtalSalario

--- src/app/IRPF.java
+++ src/app/IRPF.java
@@ -1,5 +1,15 @@
 package app;
 
 public class IRPF {
-
+	
+	float salario;
+		
+	public void cadastrarSalario(float salario) {
+		this.salario = salario;
+		
+	}
+	
+	public float getTotalSalario() {
+		return 5000; //retorno pela técnica de falsicação
+	}
 }
--- src/tst/CadastroRendimentosTest.java
+++ src/tst/CadastroRendimentosTest.java
@@ -1,19 +1,26 @@
 package tst;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+
 import app.IRPF;
 
 class CadastroRendimentosTest {
 	
+	private IRPF irpf;
+	
 	@Before
 	public void setup() {
-		IRPF irpf = new IRPF();
+		irpf = new IRPF();
 	}
 	
 	@Test
-	void testCadastrarSalario() {
-		fail("Not yet implemented");
+	public void testCadastrarSalario() {
+		irpf.cadastrarSalario(5000);
+		assertEquals(5000, irpf.getTotalSalario(), 0);
 	}
+	
+	
 
 }
