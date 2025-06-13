6de2094fb8d00a80ac4305a044371ab5c28226e2
storch7 <guilhermestorch7@gmail.com>
2025-06-12 21:47:18 -0300

    Criação do caso de teste JUnit e Criação da classe IRPF

---
+++ src/tst/CadastroRendimentosTest.java
@@ -0,0 +1,19 @@
+package tst;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import app.IRPF;
+
+class CadastroRendimentosTest {
+	
+	@Before
+	public void setup() {
+		IRPF irpf = new IRPF();
+	}
+	
+	@Test
+	void testCadastrarSalario() {
+		fail("Not yet implemented");
+	}
+
+}
